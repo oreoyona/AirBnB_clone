a4da3bae2b1dd30e4e66dc6c69c57780f6dbfe9e
Gloire Chabu <cortesgloire@gmail.com>
2023-02-06 20:42:32 -0800

    created the BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ Defines the base class model"""
+
 import uuid
 import datetime
+
 class BaseModel():
     """ the base model to create objects in our app"""
     id = ""
@@ -13,4 +15,12 @@ class BaseModel():
         self.id = str(uuid.uuidr4())
         self.created_at = datetime.datetime
         self.updated_at = datetime.datetime
+
+    def save(self):
+        """updates the public instance attribute updated_at with the current datetime"""
+        self.updated_at = datetime.datetime
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__ of the instanc"""
+        pass
     
