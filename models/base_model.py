c98a24ed5031a801afe6ffa89625a6d1ef864c68
Gloire Chabu <cortesgloire@gmail.com>
2023-02-12 10:37:03 -0800

    fixed base_model

--- models/base_model.py
+++ models/base_model.py
@@ -22,32 +22,38 @@ class BaseModel():
         representation of a class'attr
         """
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now().isoformat()
-        self.updated_at = datetime.datetime.now().isoformat()
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
         if kwargs is not None and len(kwargs) > 0:
             for k, v in kwargs.items():
                 if k == "__class__":
                     continue
                 elif k in ["created_at", "updated_at"]:
-                    setattr(self, k, datetime.fromisoformat(v))
+                    setattr(self, k, datetime.datetime.fromisoformat(v))
                 else:
                     setattr(self, k, v)
-            else:
-                pass
+        else:
+            models.storage.new(self)
 
     def save(self):
         """
         updates the public instance\
         attribute updated_at with the current datetime
         """
-        self.updated_at = datetime.datetime.now().isoformat()
+        self.updated_at = datetime.datetime.now()
+        models.storage.new(self)
 
     def to_dict(self):
         """returns a dictionary\
         containing all keys/values of __dict__ of the instanc"""
-        dico = self.__dict__
-        dico['__class__'] = self.__class__.__name__
-        return(dico)
+        tmp = (
+            {
+                k: (v.isoformat() if isinstance(v, datetime.datetime) else v)
+                for (k, v) in self.__dict__.items()
+            }
+        )
+        tmp["__class__"] = self.__class__.__name__
+        return tmp
 
     def __str__(self):
         """
