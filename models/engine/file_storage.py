bfc7abe6e48c4827d965516e5ed810963ef4c883
Gloire Chabu <cortesgloire@gmail.com>
2023-02-12 13:48:52 -0800

    first attempt

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -32,12 +32,14 @@ class FileStorage:
 
     def reload(self):
         """Reloads the JSON file"""
-        try:
-            with open(FileStorage.__file_path) as f:
-                objdict = json.load(f)
-                for o in objdict.values():
-                    cls_name = o["__class__"]
-                    del o["__class__"]
-                    self.new(eval(cls_name)(**o))
-        except FileNotFoundError:
-            return
+        if (os.path.isfile(self.__file_path)
+                and os.path.getsize(self.__file_path) > 0):
+            with open(self.__file_path, 'r') as f:
+                self.__objects = {k: self.get_class(k.split(".")[0])(**v)
+                                  for k, v in json.load(f).items()}
+
+    def get_class(self, name):
+        """ returns a class from models module using its name"""
+        sub_module = re.sub('(?!^)([A-Z]+)', r'_\1', name).lower()
+        module = importlib.import_module(f"models.{sub_module}")
+        return getattr(module, name)
