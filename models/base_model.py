16eca817057b7d4d4c937e0a90486146a6c986a6
Gloire Chabu <cortesgloire@gmail.com>
2023-02-12 13:12:01 -0800

    hot fix

--- models/base_model.py
+++ models/base_model.py
@@ -32,12 +32,9 @@ class BaseModel():
             models.storage.new(self)
 
     def save(self):
-        """
-        updates the public instance\
-        attribute updated_at with the current datetime
-        """
+        """updates the class """
         self.updated_at = datetime.today()
-        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """returns a dictionary\
