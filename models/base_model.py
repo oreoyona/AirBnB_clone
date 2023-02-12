b452b2b654d64552370262d1956fea49439de440
Gloire Chabu <cortesgloire@gmail.com>
2023-02-12 12:56:32 -0800

    modified the first clkass

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ This modules Defines the base class model
 """
 
 import uuid
-import datetime
+from datetime import datetime
 import models
 
 
@@ -18,18 +18,16 @@ class BaseModel():
             **kwargs(dict) key/value
         representation of a class'attr
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
-
-        if kwargs is not None and len(kwargs) > 0:
+        d_format = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
             for k, v in kwargs.items():
-                if k == "__class__":
-                    continue
-                elif k in ["created_at", "updated_at"]:
-                    setattr(self, k, datetime.datetime.fromisoformat(v))
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, d_format)
                 else:
-                    setattr(self, k, v)
+                    self.__dict__[k] = v
         else:
             models.storage.new(self)
 
@@ -38,18 +36,15 @@ class BaseModel():
         updates the public instance\
         attribute updated_at with the current datetime
         """
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.today()
         models.storage.new(self)
 
     def to_dict(self):
         """returns a dictionary\
         containing all keys/values of __dict__ of the instanc"""
-        tmp = (
-            {
-                k: (v.isoformat() if isinstance(v, datetime.datetime) else v)
-                for (k, v) in self.__dict__.items()
-            }
-        )
+        tmp = self.__dict__.copy()
+        tmp["created_at"] = self.created_at.isoformat()
+        tmp["updated_at"] = self.updated_at.isoformat()
         tmp["__class__"] = self.__class__.__name__
         return tmp
 
